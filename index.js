109b63dcd559a04e448a5f46974a07b7c7379891
Porshcoder <nditigana01@gmail.com>
2023-02-06 14:54:09 +0100

    final project complition

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const display = document.querySelector('.display');
+const display = document.querySelector('#display');
 const button = document.querySelectorAll('button');
 
-button.forEach((item)=>{
-    item.onclick = ()=>{
-        if(item.id == 'clear') {
+button.forEach((item) => {
+    item.onclick = () => {
+        if (item.id == 'clear') {
             display.innerText = '';
-        }else if(item.id=='') {
-            let string = display.innerText.tostring();
+        } else if (item.id == 'backspace') {
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length - 1);
+        } else if (display.innerText != '' && item.id == 'equal') {
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == '' && item.id == 'equal') {
+            display.innerText = 'Empty!';
+            setTimeout(() => (display.innerText = ''), 2000);
+        } else {
+            display.innerText += item.id;
         }
     }
 })
+
+const themeTogglerBtn = document.querySelector('.theme-toggler');
+const calculator = document.querySelector('.calculator');
+const togglerIcon = document.querySelector('.toggler-icon');
+let isDark = true;
+themeTogglerBtn.onclick = () => {
+    calculator.classList.toggle('dark');
+    themeTogglerBtn.classList.toggle('active');
+    isDark = !isDark;
+}
